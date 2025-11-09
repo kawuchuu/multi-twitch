4bbb65c538e0ad5c967accca58d0940ad02dc46f
kawuchuu <kawuchuu@users.noreply.github.com>
2025-11-10 00:01:47 +1100

    initial

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import TwitchPlayer from "@/components/TwitchPlayer.vue";
+</script>
+
+<template>
+  <main class="app-container">
+    <div class="video-container">
+      <TwitchPlayer channel="ironmouse"/>
+      <TwitchPlayer channel="AlveusSanctuary"/>
+    </div>
+    <div class="sidebar">
+      <div class="controls">
+        <p>MultiTwitch</p>
+      </div>
+      <iframe src="https://www.twitch.tv/embed/ironmouse/chat?parent=localhost&darkpopout" width="100%" height="100%"></iframe>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.app-container {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 340px;
+  grid-template-rows: 1fr;
+}
+
+.sidebar {
+  background-color: var(--sub-bg);
+  border-left: 1px solid var(--bd);
+  display: grid;
+  grid-template-rows: 50px 1fr;
+}
+
+.sidebar .controls {
+  background: var(--sub-bg);
+  border-bottom: solid 1px var(--bd);
+}
+
+.sidebar iframe {
+  border: none;
+}
+
+.video-container {
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 1.5fr 1fr;
+}
+</style>
---
+++ src/components/TwitchPlayer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {onMounted} from "vue";
+
+const props = defineProps<{
+  channel: string;
+}>();
+
+onMounted(() => {
+  const player = new window.Twitch.Player(`twitchPlayer-${props.channel}`, {
+    width: '100%',
+    height: '100%',
+    channel: props.channel,
+    parent: 'localhost'
+  });
+})
+</script>
+
+<template>
+  <div class="twitch-player" :id="`twitchPlayer-${channel}`"/>
+</template>
+
+<style scoped>
+.twitch-player {
+  width: 100%;
+  height: 100%;
+}
+</style>
